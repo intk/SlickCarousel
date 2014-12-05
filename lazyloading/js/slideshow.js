6dbcdd82e82650f48bf5600c8c9b91c5031a6d11
André Gonçalves <andreslb1@gmail.com>
2014-12-05 11:26:38 +0100

    init

---
+++ lazyloading/js/slideshow.js
@@ -0,0 +1,14 @@
+/* ------------------------------------------------------------------------------
+    S L I D E S H O W - E N H A N C E M E N T S
+--------------------------------------------------------------------------------- */
+$(document).ready(function() {
+	$(".slideshow").slick({
+		accessibility: true,
+		dots: true,
+		infinite: true,
+		speed: 500,
+		slidesToShow: 1,
+		lazyLoad: 'progressive',
+		adaptiveHeight: false
+	});
+});
